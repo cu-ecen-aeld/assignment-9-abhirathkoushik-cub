3887b1138439b5d8d0af379a2593ade7000306b4
abhirathkoushik-cub <abhirath.koushik@colorado.edu>
2025-02-09 10:38:13 -0700

    minor changes in bash file

--- clean.sh
+++ clean.sh
@@ -1,2 +1,17 @@
 #!/bin/sh
+
+#  Copyright (C) 2025 by Abhirath Koushik
+
+#  Redistribution, modification or use of this software in source or binary
+#  forms is permitted as long as the files maintain this copyright. Users are
+#  permitted to modify this and use it to learn about the field of embedded
+#  software. Abhirath Koushik and the University of Colorado are not liable for
+#  any misuse of this material.
+
+#  @file    Makefile
+#  @brief   This script is used to automate the distclean step
+
+#  @author  Abhirath Koushik
+#  @date    02-09-2025
+
 cd buildroot && make distclean
