29981f91ba504a490d2ff3c368cf1711514f9840
abhirathkoushik-cub <abhirath.koushik@colorado.edu>
2025-03-16 23:02:52 -0600

    minor changes in makefile

--- base_external/package/aesdchar/aesdchar.mk
+++ base_external/package/aesdchar/aesdchar.mk
@@ -13,6 +13,7 @@ AESDCHAR_SITE_METHOD = git
 AESDCHAR_GIT_SUBMODULES = YES
 AESDCHAR_MODULE_SUBDIRS = aesd-char-driver
  
+ 
 define AESDCHAR_INSTALL_TARGET_CMDS
  	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin
  	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin
